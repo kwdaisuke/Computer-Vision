923f3efaf6fec928d7f1a267cf3a7ffd81871de3
kwdaisuke <daisuke0582@gmail.com>
2021-02-19 09:51:23 +0900

    first commit]

---
+++ RetinaNet/layers/.ipynb_checkpoints/__init__-checkpoint.py
@@ -0,0 +1,3 @@
+  
+from ._misc import RegressBoxes, UpsampleLike, Anchors, ClipBoxes  # noqa: F401
+from .filter_detections import FilterDetections  # noqa: F401
---
+++ myretina/.ipynb_checkpoints/RetinaNet-checkpoint.py
@@ -0,0 +1,71 @@
+import os
+import re
+import zipfile
+
+import utils
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+
+import matplotlib.pyplot as plt
+import tensorflow_datasets as tfds
+
+
+url = "https://github.com/srihari-humbarwadi/datasets/releases/download/v0.1.0/data.zip"
+filename = os.path.join(os.getcwd(), "data.zip")
+keras.utils.get_file(filename, url)
+
+
+with zipfile.ZipFile("data.zip", "r") as z_fp:
+    z_fp.extractall("./")
+    
+
+def get_backbone():
+    """Builds ResNet50 with pre-trained imagenet weights"""
+    backbone = keras.applications.ResNet50(
+        include_top=False, input_shape=[None, None, 3]
+    )
+    c3_output, c4_output, c5_output = [
+        backbone.get_layer(layer_name).output
+        for layer_name in ["conv3_block4_out", "conv4_block6_out", "conv5_block3_out"]
+    ]
+    return keras.Model(
+        inputs=[backbone.inputs], outputs=[c3_output, c4_output, c5_output]
+    )
+
+
+class FeaturePyramid(tf.keras.Model):
+    """Builds the Feature Pyramid with the feature maps from the backbone.
+
+    Attributes:
+      num_classes: Number of classes in the dataset.
+      backbone: The backbone to build the feature pyramid from.
+        Currently supports ResNet50 only.
+    """
+
+    def __init__(self, backbone=None, **kwargs):
+        super(FeaturePyramid, self).__init__(name="FeaturePyramid", **kwargs)
+        self.backbone = backbone if backbone else get_backbone()
+        self.conv_c3_1x1 = keras.layers.Conv2D(256, 1, 1, "same")
+        self.conv_c4_1x1 = keras.layers.Conv2D(256, 1, 1, "same")
+        self.conv_c5_1x1 = keras.layers.Conv2D(256, 1, 1, "same")
+        self.conv_c3_3x3 = keras.layers.Conv2D(256, 3, 1, "same")
+        self.conv_c4_3x3 = keras.layers.Conv2D(256, 3, 1, "same")
+        self.conv_c5_3x3 = keras.layers.Conv2D(256, 3, 1, "same")
+        self.conv_c6_3x3 = keras.layers.Conv2D(256, 3, 2, "same")
+        self.conv_c7_3x3 = keras.layers.Conv2D(256, 3, 2, "same")
+        self.upsample_2x = keras.layers.UpSampling2D(2)
+
+    def call(self, images, training=False):
+        c3_output, c4_output, c5_output = self.backbone(images, training=training)
+        p3_output = self.conv_c3_1x1(c3_output)
+        p4_output = self.conv_c4_1x1(c4_output)
+        p5_output = self.conv_c5_1x1(c5_output)
+        p4_output = p4_output + self.upsample_2x(p5_output)
+        p3_output = p3_output + self.upsample_2x(p4_output)
+        p3_output = self.conv_c3_3x3(p3_output)
+        p4_output = self.conv_c4_3x3(p4_output)
+        p5_output = self.conv_c5_3x3(p5_output)
+        p6_output = self.conv_c6_3x3(c5_output)
+        p7_output = self.conv_c7_3x3(tf.nn.relu(p6_output))
+        return p3_output, p4_output, p5_output, p6_output, p7_output
