cbc854747173d0518b2618c4d8ef0db124446cf6
daisuke <daisuke0582@gmail.com>
2021-02-20 02:22:58 +0900

    int

---
+++ RetinaNet/initializers.py
@@ -0,0 +1,22 @@
+from tensorflow import keras
+
+import math
+
+
+class PriorProbability(keras.initializers.Initializer):
+    """ Apply a prior probability to the weights.
+    """
+
+    def __init__(self, probability=0.01):
+        self.probability = probability
+
+    def get_config(self):
+        return {
+            'probability': self.probability
+        }
+
+    def __call__(self, shape, dtype=None):
+        # set bias to -log((1 - p)/p) for foreground
+        result = keras.backend.ones(shape, dtype=dtype) * -math.log((1 - self.probability) / self.probability)
+
+        return result
